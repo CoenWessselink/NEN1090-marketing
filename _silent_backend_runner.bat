834db2ea8bca983b347710f76ba021bd5e6ae9fc
Tasche Staalbouw -C. Wesselink <c.wesselink@taschestaalbouw.nl>
2026-03-06 15:00:25 +0100

    Apply final clean API package

--- _silent_backend_runner.bat
+++ _silent_backend_runner.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 setlocal EnableExtensions
 
@@ -30,36 +29,3 @@ python -m pip install psycopg2-binary
 python -m alembic upgrade head
 
 python -m uvicorn app.main:app --host 127.0.0.1 --port 8001
-=======
-@echo off
-setlocal EnableExtensions
-
-cd /d "%~dp0"
-
-echo ===== BACKEND RUNNER START %DATE% %TIME% =====
-echo PWD: %CD%
-echo PY:  %LocalAppData%\Programs\Python\Python312\python.exe
-
-set "PY=%LocalAppData%\Programs\Python\Python312\python.exe"
-if not exist "%PY%" set "PY=%LocalAppData%\Programs\Python\Python311\python.exe"
-if not exist "%PY%" set "PY=%LocalAppData%\Programs\Python\Python310\python.exe"
-
-if not exist "%PY%" (
-  echo ERROR: Python not found in LocalAppData\Programs\Python
-  exit /b 1
-)
-
-if not exist "venv" (
-  "%PY%" -m venv venv
-)
-
-call venv\Scripts\activate
-
-python -m pip install --upgrade pip setuptools wheel
-python -m pip install -r requirements.txt
-python -m pip install psycopg2-binary
-
-python -m alembic upgrade head
-
-python -m uvicorn app.main:app --host 127.0.0.1 --port 8001
->>>>>>> d1041ea94a192ff91943ffd7c72ff1462eefc385
